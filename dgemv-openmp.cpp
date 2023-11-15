42bd75e0b60f8b3e0f38acd9d85f7a2dfe22f660
Scott McMann <57707915+SMcMann@users.noreply.github.com>
2023-11-14 21:06:36 -0800

    REVERT

--- dgemv-openmp.cpp
+++ dgemv-openmp.cpp
@@ -25,18 +25,12 @@ void my_dgemv(int n, double* A, double* x, double* y) {
    // insert your dgemv code here. you may need to create additional parallel regions,
    // and you will want to comment out the above parallel code block that prints out
    // nthreads and thread_id so as to not taint your timings
-  int nthreads = omp_get_num_threads();    
-   #pragma omp parallel num_threads(nthreads)
-   {
-    int thread_id = omp_get_thread_num();
-
-    for (int i = thread_id; i < n; i+=nthreads) {
-      for (int j = 0; j < n; j++) {
-        y[i] += A[i * n + j] * x[j];
-      }
-    }    
-   }
-
+   #pragma omp parallel for
+   for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      y[i] += A[i * n + j] * x[j];
+    }
+  }
 
 }
 
